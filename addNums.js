53dca0ffa349578a39db8894313e41268bc7ec01
Ales717 <prosenjak.ales77@gmail.com>
2024-03-19 10:17:01 +0100

    first commit

---
+++ addNums.js
@@ -0,0 +1,5 @@
+function addNums(a, b){
+    return a + b
+}
+
+export { addNums }
---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
